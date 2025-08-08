956a27da8d970cc6228c1501d508d9ceae7569cd
piyush.1 <piyush.1@infoedge.com>
2025-08-09 04:03:16 +0530

    Handling multiple routes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,22 @@ const express = require('express');
 
 const app = express();
 
-// Order of routes is important
+// Order of routes is important in the routes
+
+
+// /getUser with multiple handlers
+
+app.get(
+    "/getUser",
+    [(req, res, next) => {
+      console.log("handling 1st handler");
+      next()
+    },
+    (req, res) => {
+      console.log("handling 2st handler");
+      res.send("Send response back to server");
+    }]
+  );
 
 app.get('/user', (req, res) => {
     res.send({
@@ -10,6 +25,23 @@ app.get('/user', (req, res) => {
         "lastname": "Kumar"
     })
 })
+
+app.get('/user/:userId', (req, res) => {
+	console.log(req.params) // {userId : 1002}
+	console.log(req.query) // {userIcon : cat}
+
+    res.send(`user data for ${req.params.userId} and icon is ${req.query.userIcon}`)
+})
+
+// /user OR /uer  (s is optional)
+app.get(/^\/u(s)?er$/, (req, res) => {
+    res.send('Matched /user or /uer');
+});
+
+// /user, /usser, /usssser ... (one or more s)
+app.get(/^\/us+er$/, (req, res) => {
+    res.send('Matched /user, /usser, /usssser, etc.');
+});
 
 app.post('/user', (req, res) => {
     // save data to DB
