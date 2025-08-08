52f0972a52da460e7c24769064d1a8892f46d72a
piyush.1 <piyush.1@infoedge.com>
2025-08-09 00:41:07 +0530

    http methods handling

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("This is the Test url");
+// Order of routes is important
+
+app.get('/user', (req, res) => {
+    res.send({
+        "firstname": "Raj",
+        "lastname": "Kumar"
+    })
 })
 
-app.use("/hello", (req, res) => {
-    res.send("This is hello from the server");
+app.post('/user', (req, res) => {
+    // save data to DB
+    res.send('Data saved successfully')
 })
 
-app.use("/" , (req, res) => {
-    res.send("This is the base url");
+app.delete('/user', (req, res) => {
+    // delete data from DB
+    res.send('deleted the data successfully')
+})
+
+app.use('/' , (req, res) => {
+    res.send('This is the base url');
 })
 
 app.listen(7777, () => {
-    console.log("app is running on port 7777")
+    console.log('app is running on port 7777')
 })
